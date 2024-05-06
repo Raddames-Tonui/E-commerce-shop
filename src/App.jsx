3a408245d31d579441d26441b782da6706f24008
Raddames <raddamestonui48@gmail.com>
2024-05-06 22:59:03 +0300

    navbars

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './Layout';
+import Home from './Pages/Home';
+import Products from './Pages/Products';
+import LayoutAdmin from './LayoutAdmin';
+import AddProducts from './Pages/AddProducts';
+import AdminHome from './Pages/AdminHome';
+
+
 
 function App() {
   return (
-    <div className='bg-green-600'>Hello world</div>
+    <BrowserRouter>
+        <Routes >
+          <Route path='/' element={<Layout/>}>
+              <Route path='/' element={<Home/>}/>
+              <Route path='/Products' element={<Products/>}/>
+          </Route>
+          <Route path='/admin' element={<LayoutAdmin/>} >
+              <Route path='/admin' element={<AdminHome/>} />
+              <Route path='/admin/AddProducts' element={<AddProducts/>} />                         
+          </Route>
+        </Routes>
+    </BrowserRouter>
   )
 }
 
