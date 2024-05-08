f2ebdd4bc7fec8a4a4055399cb83ad64435dcfed
Michelle <nyamburamichelle400@gmail.com>
2024-05-08 19:32:21 +0300

    adds functions

---
+++ src (copy)/Layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavbarUser from './components/NavbarUser';
+
+function Layout() {
+  return (
+    <div>
+      <NavbarUser />
+    </div>
+  );
+}
+
+export default Layout;
---
+++ src (copy)/Pages/Orders.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Orders() {
+  return (
+    <div className='bg-[#F0F0F0] h-screen'>
+      
+      <div className='bg-[#C5C5C5] w-11 h-11' > this is the box</div>
+    </div>
+
+  )
+}
+
+export default Orders
