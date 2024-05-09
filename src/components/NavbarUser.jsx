52760476428337999858a41b74aa7445e2e16789
Raddames <raddamestonui48@gmail.com>
2024-05-09 11:22:50 +0300

    brandd]d

--- src/components/NavbarUser.jsx
+++ src/components/NavbarUser.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Product from './Product';
 import Cart from '../Pages/Cart';
+import { Link } from 'react-router-dom';
 
 function NavbarUser() {
   return (
@@ -11,7 +12,7 @@ function NavbarUser() {
             <span className="self-center text-2xl font-bold whitespace-nowrap dark:text-white">Tech<span className='text-white'>Nest</span></span>
           </a>
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-            <a href="#" type="button" className="text-white bg-black hover:bg-[#7C15AF] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-[#7C15AF] dark:hover:bg-blue-700 dark:focus:ring-blue-800">Admin View</a>
+            <Link to="/admin" type="button" className="text-white bg-black hover:bg-[#7C15AF] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-[#7C15AF] dark:hover:bg-blue-700 dark:focus:ring-blue-800">Admin View</Link>
             <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
               <span className="sr-only">Open main menu</span>
               <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
